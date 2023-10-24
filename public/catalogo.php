19c7fe54cfd442b101aa78916a73666e67a7d15d
Rene <renesalvador518@gmail.com>
2023-10-24 13:34:33 -0600

    valiendo verga :(

--- public/catalogo.php
+++ public/catalogo.php
@@ -1,28 +1,42 @@
-<?php
-// Realiza una consulta a la base de datos para obtener la lista de productos
-require_once('../includes/db.php');
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda en Línea - Catálogo de Productos</title>
+    <link rel="stylesheet" type="text/css" href="../css/catalogo.css">
+</head>
+<body>
+    <h1>Catálogo de Productos</h1>
 
-$query = "SELECT * FROM productos";
-$result = $conn->query($query);
+    <div class="catalogo-container">
+        <?php
+        // Realiza una consulta a la base de datos para obtener la lista de productos
+        require_once('../connection/db.php');
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        // Muestra cada producto en una tarjeta
-        echo "<div class='producto-card'>";
-        echo "<img src='images/" . $row['imagen'] . "' alt='" . $row['nombre'] . "'>";
-        echo "<h2>" . $row['nombre'] . "</h2>";
-        echo "<p>" . $row['descripcion'] . "</p>";
-        echo "<p>Precio: $" . $row['precio'] . "</p>";
-        echo "<p>Stock Disponible: " . $row['stock'] . "</p>";
-        echo "<a href='producto.php?id=" . $row['id'] . "'>Ver Detalles</a>";
-        echo "<button>Agregar al Carrito</button>";
-        echo "</div>";
-    }
-} else {
-    echo "No hay productos disponibles en el catálogo.";
+        $query = "SELECT * FROM productos";
+        $result = $conn->query($query);
 
-}
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                // Muestra cada producto en una tarjeta
+                echo "<div class='producto-card'>";
+                echo "<img src='images/" . $row['imagen'] . "' alt='" . $row['nombre'] . "'>";
+                echo "<h2>" . $row['nombre'] . "</h2>";
+                echo "<p>" . $row['descripcion'] . "</p>";
+                echo "<p>Precio: $" . $row['precio'] . "</p>";
+                echo "<p>Stock Disponible: " . $row['stock'] . "</p>";
+                echo "<a href='producto.php?id=" . $row['id'] . "'>Ver Detalles</a>";
+                echo "<button>Agregar al Carrito</button>";
+                echo "</div>";
+            }
+        } else {
+            echo "No hay productos disponibles en el catálogo.";
+        }
 
-$conn->close();
-?>
+        $conn->close();
+        ?>
+    </div>
+</body>
+</html>
 
--- public/login.php
+++ public/login.php
@@ -1,13 +1,22 @@
 <?php
+session_start();
+
+// Si el usuario ya inició sesión, lo redirigimos al catálogo
+if (isset($_SESSION['usuario_id'])) {
+    header("Location: catalogo.php");
+    exit;
+}
+
+$error_message = "";
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Procesa el formulario de inicio de sesión
     $email = $_POST['email'];
     $contrasena = $_POST['contrasena'];
 
-    // Aquí debes implementar la lógica de autenticación
-    require_once(__DIR__ . '/../includes/db.php');
+    // Considerando que ya tienes un archivo db.php con tu conexión
+    require_once(__DIR__ . '/../connection/db.php');
 
-    // Supongamos que tienes una tabla "usuarios" en tu base de datos
+    // Aquí he adaptado la consulta a tu estructura de tabla
     $query = "SELECT id, contrasena FROM usuarios WHERE email = ?";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("s", $email);
@@ -18,22 +27,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt->bind_result($usuario_id, $contrasena_hash);
         $stmt->fetch();
 
-        // Verifica la contraseña usando password_verify
         if (password_verify($contrasena, $contrasena_hash)) {
-            // Inicio de sesión exitoso
-            // Puedes redirigir al usuario a su área personal o mostrar un mensaje de bienvenida
-            echo "<p class='success-message'>Bienvenido, has iniciado sesión con éxito.</p>";
+            $_SESSION['usuario_id'] = $usuario_id;
+            header("Location: catalogo.php"); // Redirigiendo a catalogo.php
+            exit;
         } else {
-            echo "<p class='error-message'>Contraseña incorrecta. Inténtalo de nuevo.</p>";
+            $error_message = "Contraseña incorrecta. Inténtalo de nuevo.";
         }
     } else {
-        echo "<p class='error-message'>Usuario no encontrado. Regístrate si eres nuevo.</p>";
+        $error_message = "Usuario no encontrado. Regístrate si eres nuevo.";
     }
 
     $stmt->close();
     $conn->close();
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tienda en Línea - Inicio de Sesión</title>
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <h2>Iniciar Sesión</h2>
+            <?php
+            if (!empty($error_message)) {
+                echo "<p style='color: red;'>$error_message</p>";
+            }
+            ?>
+            <form class='login-form' method='post' action='login.php'>
+                <div class='input-group'>
+                    <input type='email' id='email' name='email' placeholder='Email' required>
+                </div>
+                <div class='input-group'>
+                    <input type='password' id='contrasena' name='contrasena' placeholder='Contraseña' required>
+                </div>
+                <button class='login-button' type='submit'>Iniciar Sesión</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
+
 
 
 
